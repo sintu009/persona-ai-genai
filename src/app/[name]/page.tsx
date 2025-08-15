3f260c38584c92fc1fb072d41aed6322ac4bd6a1
Eternal-Allies <eternalallies.au@gmail.com>
2025-08-15 12:38:01 +0530

    bug fix

--- src/app/[name]/page.tsx
+++ src/app/[name]/page.tsx
@@ -13,6 +13,7 @@ import ReactMarkdown from "react-markdown";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { tomorrow } from "react-syntax-highlighter/dist/esm/styles/prism";
 import remarkGfm from "remark-gfm";
+import { ComponentPropsWithoutRef } from "react";
 
 const HiteshSirPage = () => {
   const { name } = useParams();
@@ -137,13 +138,20 @@ const HiteshSirPage = () => {
                           <ReactMarkdown
                             remarkPlugins={[remarkGfm]}
                             components={{
-                              code({ inline, className, children, ...props }) {
+                              code({
+                                inline,
+                                className,
+                                children,
+                                ...props
+                              }: ComponentPropsWithoutRef<"code"> & {
+                                inline?: boolean;
+                              }) {
                                 const match = /language-(\w+)/.exec(
                                   className || ""
                                 );
                                 return !inline && match ? (
                                   <SyntaxHighlighter
-                                    style={tomorrow}
+                                    style={tomorrow as any}
                                     language={match[1]}
                                     PreTag="div"
                                     {...props}
